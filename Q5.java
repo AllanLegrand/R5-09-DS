d281e8178e8113696871e6dc8cb4133fb578f7ca
AllanLegrand <allan.legrand16@gmail.com>
2024-11-20 13:42:10 +0000

    Création de Q5_archive_date.txt et Q5.java

--- Q5.java
+++ Q5.java
@@ -1,20 +1,17 @@
 public class Q5 {
 	public static void main(String[] args) {
-		int n = 0;
 		try {
-			n = Integer.parseInt(args[0]);
+			int n = Integer.parseInt(args[0]);
 		} catch (Exception e) {
 			// TODO: handle exception
 			System.err.println("Veuillez saisir un nombre entier");
 		}
 		
-		for(int i = n-1; i >= 0; i++) {
-			for(int j = 0; j <= i; j++) {
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++) {
 				System.out.print("*");
 			}
 			System.out.println();
 		}
 	}
-}
-}
 }
