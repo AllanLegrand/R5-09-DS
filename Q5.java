295f3e1235748f16b2e07a02d1b6a05b134677ab
Allan Legrand <58049420+AllanLegrand@users.noreply.github.com>
2024-11-20 14:41:58 +0100

    inverser l’affichage du triangle

--- Q5.java
+++ Q5.java
@@ -1,17 +1,20 @@
 public class Q5 {
 	public static void main(String[] args) {
+		int n = 0;
 		try {
-			int n = Integer.parseInt(args[0]);
+			n = Integer.parseInt(args[0]);
 		} catch (Exception e) {
 			// TODO: handle exception
 			System.err.println("Veuillez saisir un nombre entier");
 		}
 		
-		for(int i = 0; i < n; i++) {
-			for(int j = 0; j < n; j++) {
+		for(int i = n-1; i >= 0; i++) {
+			for(int j = 0; j <= i; j++) {
 				System.out.print("*");
 			}
 			System.out.println();
 		}
 	}
+}
+}
 }
